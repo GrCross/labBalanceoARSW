6719688ad9bdaf48b3acb22eb33ea5190b0cde31
valentina <valesiabatto@gmail.com>
2019-04-24 19:06:19 -0500

    acceder al valor por application.properties en springboot
    Co-authored-by: GrCross <rosalesdaniel350@gmail.com>

--- src/main/java/edu/eci/arsw/collabpaint/CollabPaintWebSocketConfig.java
+++ src/main/java/edu/eci/arsw/collabpaint/CollabPaintWebSocketConfig.java
@@ -21,6 +21,8 @@ package edu.eci.arsw.collabpaint;
  * @author hcadavid
  */
 import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.util.AntPathMatcher;
@@ -32,9 +34,15 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @EnableWebSocketMessageBroker
 public class CollabPaintWebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
+    @Value("${server}")
+    private String server;
+    
+    @Value("${port}")
+    private int port;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableStompBrokerRelay("/topic/").setRelayHost("192.168.56.103").setRelayPort(61613);
+        config.enableStompBrokerRelay("/topic/").setRelayHost(server).setRelayPort(port);
         config.setApplicationDestinationPrefixes("/app");        
     }
 
